3fec7225fc049c703f4965e95c34035594b4908e
Xenia <platoshka_ks@mail.ru>
2023-07-07 18:13:53 +0700

    Pt_2_9: position of the maximum digit
    the position of the digit in the number from the end and from the beginning

---
+++ Pt_2_9.py
@@ -0,0 +1,8 @@
+a = int(input('Введите натуральное число, в котором все цифры различны: '))
+a = str(a)
+arr = []
+for i in a:
+ arr.append(i)
+print(arr.index(max(arr))+1)
+arr.reverse()
+print(arr.index(max(arr))+1)
