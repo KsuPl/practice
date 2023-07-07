48739e6b4ffd469ca8f4236e266582e7f74ca8c5
Xenia <platoshka_ks@mail.ru>
2023-07-07 17:22:29 +0700

    Pt_2_5: heads or tails game
    The game ends after three losses in a row

---
+++ Pt_2_5.py
@@ -0,0 +1,18 @@
+import random
+n=0
+k=''
+while not '000' in k:
+ a=int(input('Орёл-0 или решка-1: '))
+ i=random.randint(0,1)
+ if a==1 or a==0:
+  if i==a:
+   k+='1'
+   print('Выиграл')
+   print('Число выигрышей: ',k.count('1'),'Число проигрышей: ',k.count('0'))
+  else:
+   k+='0'
+   print('Проиграл')
+   print('Число выигрышей: ',k.count('1'),'Число проигрышей: ',k.count('0'))
+ else:
+  break
+print('Игра завершена')
