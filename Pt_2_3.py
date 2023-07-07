eea7f230c93ca4976976cdc89c60cf27dd37256e
Xenia <platoshka_ks@mail.ru>
2023-07-07 14:07:43 +0700

    Pt_2_3: is the number an armstrong number

---
+++ Pt_2_3.py
@@ -0,0 +1,11 @@
+a=int(input('Введите число: '))
+sum=0
+temp=a
+while temp>0:
+  n=temp%10
+  sum+=n**3
+  temp//=10
+if a==sum:
+  print(a,'является числом Армстронга')
+else:
+  print(a,'не является числом Армстронга')
