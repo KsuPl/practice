179a8b0dc497947ad0e7a8a6b511ac0249ffaadd
Xenia <platoshka_ks@mail.ru>
2023-07-08 14:44:23 +0700

    Pt_3_7: dictionary with letters
    keys - letters
    value True if the letter is a vowel
    value False if the letter is a consonant

---
+++ Pt_3_7.py
@@ -0,0 +1,6 @@
+s = str(input('Введите предложение: '))
+s_list = list(s.replace(" ", ""))
+my_dict = {s_list[i]: (True if s_list[i] in 'ауоыиэяюёе' else False) for i in range(len(s_list))}
+print(my_dict)
+
+
