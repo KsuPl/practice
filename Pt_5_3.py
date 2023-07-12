c8838bd07947c0239d8c043df44bc0218553de12
Xenia <platoshka_ks@mail.ru>
2023-07-12 16:37:59 +0700

    Pt_5_3:search for books by year of release
    The user enters the initial and final year of release. The program checks whether books exist in the specified range. if there is: displays a list of books if it does not exist: outputs "Книги не найдены"

---
+++ Pt_5_3.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_csv('books.csv')
+y1 = int(input('Введите начальный год: '))
+y2 = int(input('Введите конечный год: '))
+if max(df['Год выпуска'].astype(int)) >= y1 and min(df['Год выпуска'].astype(int)) <= y2:
+    print(df.loc[(df['Год выпуска'].astype(int) >= y1) & (df['Год выпуска'].astype(int) <= y2)])
+else:
+    print('Книги не найдены')
