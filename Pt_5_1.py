30577da05f0609d20ba21c494eed383d50b1a9e1
Xenia <platoshka_ks@mail.ru>
2023-07-12 13:54:49 +0700

    Pt_5_1: creating .csv file
    using the pandas library, we create a file with a list of books (title, author, year of release)

---
+++ Pt_5_1.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+name = ['Гордость и предубеждение', 'Горе от ума', 'Великий Гэтсби', 'Триумфальная арка']
+author = ['Джейн Остин', 'А.С.Грибоедов', 'Фрэнсис Скотт Фицджеральд', 'Эрих Мария Ремарк']
+year = ['1813', '1825', '1925', '1945']
+favorite_books = {'Книга': name,
+                  'Автор': author,
+                  'Год выпуска': year}
+favorite_books_df = pd.DataFrame(favorite_books)
+print(favorite_books_df)
+favorite_books_df.to_csv('books.csv', index=False)
