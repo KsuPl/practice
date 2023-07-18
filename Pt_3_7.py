e972ddda2948f320321a99882acef8710c7698ce
Xenia <platoshka_ks@mail.ru>
2023-07-18 18:40:21 +0700

    Pt_3_7: Change by PEP8

--- Pt_3_7.py
+++ Pt_3_7.py
@@ -2,5 +2,3 @@ s = str(input('Введите предложение: '))
 s_list = list(s.replace(" ", ""))
 my_dict = {s_list[i]: (True if s_list[i] in 'ауоыиэяюёе' else False) for i in range(len(s_list))}
 print(my_dict)
-
-
