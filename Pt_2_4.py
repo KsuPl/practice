ac6f570bc22f52978a32d3e016a18c1632d513a0
Xenia <platoshka_ks@mail.ru>
2023-07-18 15:56:56 +0700

    Pt_2_4: Change by PEP8

--- Pt_2_4.py
+++ Pt_2_4.py
@@ -1,14 +1,15 @@
 import math
-a=int(input())
-b=int(input())
-c=int(input())
-d=b**2-4*a*c
-if d>0:
-  x1=(-b-math.sqrt(d))/(2*a)
-  x2=(-b+math.sqrt(d))/(2*a)
-  print('x1=',x1,'x2=',x2)
-if d==0:
-  x=-b/(2*a)
-  print('x=',x)
-if d<0:
-  print('Нет корней')
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = b ** 2 - 4 * a * c
+if d > 0:
+    x1 = (-b - math.sqrt(d)) / (2 * a)
+    x2 = (-b + math.sqrt(d)) / (2 * a)
+    print('x1=', x1, 'x2=', x2)
+if d == 0:
+    x = -b / (2 * a)
+    print('x=', x)
+if d < 0:
+    print('Нет корней')
