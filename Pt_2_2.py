c932f617e2bde7bb917aaf786aca45385e98fd79
Xenia <platoshka_ks@mail.ru>
2023-07-18 15:56:35 +0700

    Pt_2_2: Change by PEP8

--- Pt_2_2.py
+++ Pt_2_2.py
@@ -1,11 +1,11 @@
-arr=['Вечерний Ургант','Поедем поедим','Уральские пельмени','Орёл и решка']
+arr = ['Вечерний Ургант', 'Поедем поедим', 'Уральские пельмени', 'Орёл и решка']
 for i in range(len(arr)):
-  s=arr[i]
-  print(s)
-name=str(input('Введите название передачи: '))
-number=int(input('Введите номер позиции: '))
+    s = arr[i]
+    print(s)
+name = str(input('Введите название передачи: '))
+number = int(input('Введите номер позиции: '))
 print()
-arr.insert(number-1,name)
+arr.insert(number - 1, name)
 for i in range(len(arr)):
-  s=arr[i]
-  print(s)
+    s = arr[i]
+    print(s)
