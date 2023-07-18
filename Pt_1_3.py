15de180f310302bcde55b409f0cedec106c23faa
Xenia <platoshka_ks@mail.ru>
2023-07-18 12:12:49 +0700

    Pt_1_3: Change by pep8

--- Pt_1_3.py
+++ Pt_1_3.py
@@ -2,10 +2,10 @@ a = int(input())
 b = int(input())
 c = int(input())
 if a < b:
- a, b = b, a
+    a, b = b, a
 if b < c:
- b, c = c, b
+    b, c = c, b
 if a < b:
- a, b = b, a
-print('Максимальное значение:',a)
+    a, b = b, a
+print('Максимальное значение:', a)
 print(a, b, c)
